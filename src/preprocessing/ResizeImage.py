a46dea3c059b9d5cb6c3da16ff0c97c42fc0c9ff
Hilalaksoy <hll.aksy29@gmail.com>
2019-03-07 21:38:50 +0300

    Creating ResizeImage.py
    ResizeImage.py dosyası oluşturuldu.

---
+++ src/preprocessing/ResizeImage.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 21:32:32 2019
+
+@author: hilal
+"""
+import cv2
+import tensorflow as tf
+
+def resize_with_matris():
+    pass
+
+def resize_with_padding():
+    pass
+
+
+
+if __name__=="__main__":
+    pass
+
